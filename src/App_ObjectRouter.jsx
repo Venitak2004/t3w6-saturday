857c34c6d4b6be050fab1f2270b9a12c1bbba132
Venita Kissell <venitak@hotmail.com>
2024-11-26 23:44:40 +1000

    feat: Navbar with links

---
+++ src/App_ObjectRouter.jsx
@@ -0,0 +1,35 @@
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import "./styles/App.css";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+
+const testRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage/>
+    }, 
+    
+    { path: "/about",
+        element: <AboutPage/>
+    },
+    
+    { path: "/contact",
+        element: <ContactPage/>
+    }
+  ]);
+
+
+function App() {
+  return (
+    <div>
+        <RouterProvider router={testRouter} />
+    </div>
+  );
+}
+
+export default App;
