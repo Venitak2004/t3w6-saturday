44caef0704ef168bd1954bb2f16e70eb06037cc4
Venita Kissell <venitak@hotmail.com>
2024-12-19 21:12:35 +1000

    Navbar Redirect Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 import { Link, NavLink } from "react-router-dom";
-import "../styles/NavBar.css"
-
+import "../styles/Navbar.css"
 
 export default function Navbar() {
-  return (
-    <nav>
-      <h1>Example Navbar content</h1>
-      <Link to={"/"}>Home |</Link>
-      <NavLink to={"/"} className={({isActive}) => {return isActive? "active": "" }}> Home |</NavLink>
-      <NavLink to={"/about"} className={({isActive}) => {return isActive? "active": "" }}>About Me |</NavLink>
-      <NavLink to={"/contact"} className={({isActive}) => {return isActive? "active": "" }}>Contact Me |</NavLink>
-    </nav>
-  );
+    return (
+        <nav>
+            <h1>Example Navbar content</h1>
+            <Link to={"/"}>Home </Link>
+            <NavLink
+                to={"/"}
+                className={({isActive}) => { return isActive ? "active" : "" }}>
+                Home
+            </NavLink>
+            <NavLink
+                to={"/about"}
+                className={({isActive}) => { return isActive ? "active" : "" }}>
+                About Me </NavLink>
+            <NavLink
+                to={"/contact"}
+                className={({isActive}) => { return isActive ? "active" : "" }}>
+                Contact Me </NavLink>
+            <NavLink
+                to={"/projects"}
+                className={({isActive}) => { return isActive ? "active" : "" }} end>
+                My Projects </NavLink>
+            <NavLink
+                to={"/projects/terminalApp"}
+                className={({isActive}) => { return isActive ? "active" : "" }}>
+                My Terminal Application </NavLink>
+        </nav>
+    )
 }
---
+++ src/pages/terminalAppProject.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function TerminalAppProject(){
+    let { versionNumber } = useParams();
+
+    return(
+        <div>
+            <h1>My Terminal Project</h1>
+            <p>Here are a list of features of my project:</p>
+            <ul>
+                <li>Feature 1</li>
+                <li>Feature 2</li>
+                <li>Feature 3</li>
+                <li>Feature 4</li>
+            </ul>
+            {versionNumber && <p>Version Number: {versionNumber}</p>}
+        </div>
+    )
+}
